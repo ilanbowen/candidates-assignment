83a664ccd0ff0f0478f8eae780182337b3670e80
ilan bowen <ilanbowen@gmail.com>
2019-01-20 09:49:39 +0200

    Submission of solutions

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise3 here
+sed -i 's/deny from all/allow from all/g' /etc/apache2/sites-available/default
+sudo /etc/init.d/apache2 restart
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
+sudo chmod o+w /etc/hosts
+sudo echo "192.168.100.10 server1" >> /etc/hosts
